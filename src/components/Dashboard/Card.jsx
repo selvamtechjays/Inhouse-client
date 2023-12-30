31d582565e8cf320b2bb5af2003a4cf14ad25e80
selvamurugaiah <selvamurugaiah100@gmail.com>
2023-12-30 14:07:51 +0530

    dashboard.css

--- src/components/Dashboard/Card.jsx
+++ src/components/Dashboard/Card.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaFontAwesome } from 'react-icons/fa';
 import "./Dashboard.css"
+import { getallProjects } from '../../service/allapi';
 
 export function Card({ data }) {
+    const [projects, setProjects] = useState([])
+
+     // Function to call the API and get all projects
+     const getAllProjects = async () => {
+        const response = await getallProjects(projects)
+        setProjects(response.data)
+        console.log(projects);
+    }
+    let total = projects.map(a => a.projectName);
+    console.log(total)
+    getAllProjects()
+
+   
     return (
         <div className="col-xl-3 col-md-6 mb-4 " >
             <div className={`card border-left-${data.colors} shadow h-100 py-2 style-c`}>
--- src/components/Dashboard/CardList.jsx
+++ src/components/Dashboard/CardList.jsx
@@ -10,25 +10,25 @@ export function CardList() {
             title: "Total Projects",
             count: 150,
             colors: "success",
-            icon: <GrProjects />
+            icon: <GrProjects style={{backgroundColor:"#450c36"}}/>
         },
         {
             title: "Total Employees",
             count: 30,
             colors: "warning",
-            icon: <MdGroups2 />
+            icon: <MdGroups2 style={{color:"#450c36"}}/>
         },
         {
             title: "Total Clients",
             count: 350,
             colors: "info",
-            icon: <FaUsers />
+            icon: <FaUsers style={{color:"#450c36"}}/>
         },
         {
             title: "NEW Employees",
             count: "58",
             colors: "primary",
-            icon: <FaUserPlus />
+            icon: <FaUserPlus style={{color:"#450c36"}}/>
         }
     ];
 
